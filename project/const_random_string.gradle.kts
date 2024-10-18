7e6344b10f657fd2a3026221cca28f113098d73d
eitanliu <eitanliu@gmail.com>
2024-10-18 11:37:54 +0800

    update script constRandomString

--- project/const_random_string.gradle.kts
+++ project/const_random_string.gradle.kts
@@ -4,113 +4,107 @@
  *
  * Linux/Mac
  * ```bat
- * ./gradlew constSaveHistory
- * ./gradlew constRandomString
+ * ./gradlew :constSaveHistory
+ * ./gradlew :constRandomString
  * ```
  * Windows
  * ```bat
- * .\gradlew.bat constSaveHistory
- * .\gradlew.bat constRandomString
+ * .\gradlew.bat :constSaveHistory
+ * .\gradlew.bat :constRandomString
  * ```
  */
 
 fun Project?.projProp(key: String): String? =
     this?.findProperty(key)?.toString() ?: System.getProperty(key)
 
-with(project) {
-    val regex = """((?:.+))=\s?"(.*)"((?:.+)?)""".toRegex()
-    val ignoreKeys = projProp("const_ignore")?.split(",") ?: emptyList()
-    val historyFile = file("const_history.cfg")
-    val history = LinkedHashMap<String, String>()
 
-    fun readHistory(file: File) {
-        if (file.isFile) {
-            val lines = file.readLines()
-            val list = lines.mapNotNull map@{ line ->
-                // println("${line}")
-                // if (!regex.matches(line)) return@map null
-                val result = regex.find(line) ?: return@map null
-                val left = result.groups[1]?.value ?: return@map null
-                val key = left.trim()
-                if (key.startsWith("//")) return@map null
-                if (ignoreKeys.any { key == it }) return@map null
-                val value = result.groups[2]?.value?.trim()?.trim('"') ?: return@map null
-                key to value
-            }
-            list.toMap(history)
+val history = LinkedHashMap<String, String>()
+val historyFile = rootProject.file("const_history.cfg")
+val regex = """((?:.+))=\s?"(.*)"((?:.+)?)""".toRegex()
+
+val ignoreKeys = projProp("const_ignore")?.split(",") ?: emptyList()
+
+fun readHistory(file: File, history: MutableMap<String, String>) {
+    if (file.isFile) {
+        val lines = file.readLines()
+        val list = lines.mapNotNull map@{ line ->
+            // println("${line}")
+            // if (!regex.matches(line)) return@map null
+            val result = regex.find(line) ?: return@map null
+            val left = result.groups[1]?.value ?: return@map null
+            val key = left.trim()
+            if (key.startsWith("//")) return@map null
+            if (ignoreKeys.any { key == it }) return@map null
+            val value = result.groups[2]?.value?.trim()?.trim('"') ?: return@map null
+            key to value
         }
+        list.toMap(history)
     }
+}
 
-    fun randomString(length: Int): String {
-        val chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-        val value = (1..length)
-            .map { chars.random() }
-            .joinToString("")
-        if (value in history.values) return randomString(length)
-        return value
-    }
+fun randomString(length: Int, history: MutableMap<String, String>): String {
+    val chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    val value = (1..length)
+        .map { chars.random() }
+        .joinToString("")
+    if (value in history.values) return randomString(length, history)
+    return value
+}
 
-    fun replaceString(inFile: File, outFile: File) {
-        if (inFile.isFile) {
-            val lines = inFile.readLines()
-            val list = lines.map map@{ line ->
-                if (!regex.matches(line)) return@map line
-                val result = regex.find(line) ?: return@map line
-                val left = result.groups[1]?.value ?: return@map line
-                val key = left.trim()
-                if (key.startsWith("//")) return@map line
-                if (ignoreKeys.any { key == it }) return@map line
-                val comments = result.groups[3]?.value.orEmpty()
-                val value = history[key] ?: randomString(10)
-                history[key] = value
-                "$left= \"$value\"$comments"
-            }
-            outFile.parentFile?.apply { if (!isDirectory) mkdirs() }
-            outFile.writeText(list.joinToString(System.lineSeparator()))
+fun replaceString(inFile: File, outFile: File, history: MutableMap<String, String>) {
+    if (inFile.isFile) {
+        val lines = inFile.readLines()
+        val list = lines.map map@{ line ->
+            if (!regex.matches(line)) return@map line
+            val result = regex.find(line) ?: return@map line
+            val left = result.groups[1]?.value ?: return@map line
+            val key = left.trim()
+            if (key.startsWith("//")) return@map line
+            if (ignoreKeys.any { key == it }) return@map line
+            val comments = result.groups[3]?.value.orEmpty()
+            val value = history[key] ?: randomString(10, history)
+            history[key] = value
+            "$left= \"$value\"$comments"
         }
-
+        outFile.parentFile?.apply { if (!isDirectory) mkdirs() }
+        outFile.writeText(list.joinToString(System.lineSeparator()))
     }
 
-    fun constRandomString() {
-        readHistory(historyFile)
+}
 
-        val files = projProp("const_files")?.split(",")
-        files?.forEach { path ->
-            val inFile = file(path)
-            replaceString(inFile, inFile)
-        }
+fun constRandomString() {
+    readHistory(historyFile, history)
 
-        historyFile.writeText(history.toList().joinToString(System.lineSeparator()) { entry ->
-            println("${entry.first}, ${entry.second}")
-            "${entry.first} = \"${entry.second}\""
-        })
+    val files = projProp("const_files")?.split(",")
+    files?.forEach { path ->
+        val inFile = file(path)
+        replaceString(inFile, inFile, history)
     }
 
-    fun constSaveHistory() {
+    historyFile.writeText(history.toList().joinToString(System.lineSeparator()) { entry ->
+        println("${entry.first}, ${entry.second}")
+        "${entry.first} = \"${entry.second}\""
+    })
+}
 
-        readHistory(historyFile)
-        val files = projProp("const_files")?.split(",")
-        files?.forEach { path ->
-            val inFile = file(path)
-            readHistory(inFile)
-        }
+fun constSaveHistory() {
 
-        historyFile.writeText(history.toList().joinToString(System.lineSeparator()) { entry ->
-            println("${entry.first}, ${entry.second}")
-            "${entry.first} = \"${entry.second}\""
-        })
+    readHistory(historyFile, history)
+    val files = projProp("const_files")?.split(",")
+    files?.forEach { path ->
+        val inFile = file(path)
+        readHistory(inFile, history)
     }
 
-    if (
-        tasks.findByName("constRandomString") == null
-    ) tasks.register("constRandomString") {
-        doLast { constRandomString() }
-    }
+    historyFile.writeText(history.toList().joinToString(System.lineSeparator()) { entry ->
+        println("${entry.first}, ${entry.second}")
+        "${entry.first} = \"${entry.second}\""
+    })
+}
 
-    if (
-        tasks.findByName("constSaveHistory") == null
-    ) tasks.register("constSaveHistory") {
-        doLast { constSaveHistory() }
-    }
-
+tasks.apply {
+    if (tasks.findByName("constRandomString") == null)
+        register("constRandomString") { doLast { constRandomString() } }
+    if (tasks.findByName("constSaveHistory") == null)
+        register("constSaveHistory") { doLast { constSaveHistory() } }
 }
